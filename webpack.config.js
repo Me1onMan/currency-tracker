c514369ff749f6a881d56c350ea1774223812af9
Fyodor Shklyar <30theflame05@gmail.com>
2023-12-29 19:00:19 +0300

    SETTINGS: make basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+};
