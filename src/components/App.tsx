869e901cad809e1f9d61e441a944cfe64911784c
Fyodor Shklyar <30theflame05@gmail.com>
2024-01-06 20:13:57 +0300

    FIX: sort imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
+import Footer from "./Footer/index";
 import Header from "./Header/index";
 import HomeContent from "./HomeContent";
-import Footer from "./Footer/index";
 import Navbar from "./Navbar/index";
 
 export default function App() {
