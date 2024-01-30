c0f442360c7ccbde90938fb96b59ee2a5333dcb1
Fyodor Shklyar <30theflame05@gmail.com>
2024-01-30 21:33:52 +0300

    BUILD: deploy 2

---
+++ dist/src_assets_Commercial-Dollar-Icon_png.5f6ff689fcff9cbc2847.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcurrency_tracker_task_main"] = self["webpackChunkcurrency_tracker_task_main"] || []).push([["src_assets_Commercial-Dollar-Icon_png"],{
+
+/***/ "./src/assets/Commercial-Dollar-Icon.png":
+/*!***********************************************!*\
+  !*** ./src/assets/Commercial-Dollar-Icon.png ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"479f64b6dac08f0629d6934cf47fdfaf.png\");\n\n//# sourceURL=webpack://currency-tracker-task-main/./src/assets/Commercial-Dollar-Icon.png?");
+
+/***/ })
+
+}]);
